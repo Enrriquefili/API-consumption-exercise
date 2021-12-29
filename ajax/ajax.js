a3cb4585cbb3f0133520f8915ecdae0318c56598
Carlos Vega <carlosjuniorrvega@hotmail.com>
2021-12-29 12:52:11 -0500

    adjunto codigo completo javascript para consumo de api

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -2,7 +2,8 @@
 
 (()=>{
     const xhr = new XMLHttpRequest(),
-    $xhr=document.getElementById("xhr");
+    $xhr=document.getElementById("xhr"),
+    $fragment=document.createDocumentFragment();
 
     xhr.addEventListener("readystatechange",(e)=>{
         if (xhr.readyState!==4) return;
@@ -11,11 +12,19 @@
         }else{
             console.log("al parecer hubo un error");
         }
-         console.log(xhr); 
-         console.log(xhr.response);
-         let json=JSON.parse(xhr.responseText);
-         console.log(json);
+        console.log(xhr); 
+        let json=JSON.parse(xhr.response);
+        console.log(json);
+        json.forEach((el) => {
+            const $li= document.createElement("li");
+            $li.innerHTML=`${el.name}--${el.email}--${el.phone}`;
+            $fragment.appendChild($li);
 
+
+            
+        });
+
+        $xhr.appendChild($fragment);
      });
 
      xhr.open("GET","https://jsonplaceholder.typicode.com/users");
